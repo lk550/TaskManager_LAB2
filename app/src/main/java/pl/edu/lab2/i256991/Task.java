8454b54fe09efce3ce820d147e0ba5ad85293571
Luís <koelho.luis@gmail.com>
2020-04-22 20:27:02 +0100

    added task obj

---
+++ app/src/main/java/pl/edu/lab2/i256991/Task.java
@@ -0,0 +1,43 @@
+package pl.edu.lab2.i256991;
+
+public class Task {
+    String title;
+    String type;
+    String description;
+    String date;
+    String status;
+
+    Task(String title, String type, String desc, String date){
+        this.title=title;
+        this.type=type;
+        description=desc;
+        this.date=date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+}
