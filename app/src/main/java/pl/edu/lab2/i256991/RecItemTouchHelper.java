8179305da46a74ed0844d55fbe6952fc3b798e15
Luís <koelho.luis@gmail.com>
2020-04-23 21:21:46 +0100

    SWIPE 2 DELETE FUNCIONA

---
+++ app/src/main/java/pl/edu/lab2/i256991/RecItemTouchHelper.java
@@ -0,0 +1,66 @@
+package pl.edu.lab2.i256991;
+
+import android.graphics.Canvas;
+import android.view.View;
+
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    private RecyclerItemListener listener;
+
+    public RecItemTouchHelper(int dragDirs, int swipeDirs,RecyclerItemListener listener) {
+        super(dragDirs, swipeDirs);
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView,RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (viewHolder != null) {
+            final View foreground = ((TaskListAdapter.TaskViewHolder) viewHolder).foregroundLayer;
+            getDefaultUIUtil().onSelected(foreground);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        final View foregroundView = ((TaskListAdapter.TaskViewHolder) viewHolder).foregroundLayer;
+        getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        final View foregroundView = ((TaskListAdapter.TaskViewHolder) viewHolder).foregroundLayer;
+        getDefaultUIUtil().clearView(foregroundView);
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView,
+                            RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                            int actionState, boolean isCurrentlyActive) {
+        final View foregroundView = ((TaskListAdapter.TaskViewHolder) viewHolder).foregroundLayer;
+
+        getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        listener.onSwiped(viewHolder, direction, viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
+    }
+
+    public interface RecyclerItemListener {
+        void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position);
+    }
+}
